4057752fbf1694b7a6022a411a8cbdf884a06a35
James Reeves <jreeves@weavejester.com>
2017-03-08 22:33:45 +0000

    Fix project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct.database.sql.hikaricp "0.1.0-SNAPSHOT"
+(defproject duct/database.sql.hikaricp "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
