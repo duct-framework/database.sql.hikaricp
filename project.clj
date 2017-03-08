87b4ae16d8e940271f8ed15c3febc6b7b4880017
James Reeves <jreeves@weavejester.com>
2017-03-08 13:34:18 +0000

    Add Integrant key using HikariCP Clojure wrapper

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [duct/database.sql "0.1.0-SNAPSHOT"]
+                 [hikari-cp "1.7.5"]])
--- src/duct/database/sql/hikaricp.clj
+++ src/duct/database/sql/hikaricp.clj
@@ -1,6 +1,10 @@
-(ns duct.database.sql.hikaricp)
+(ns duct.database.sql.hikaricp
+  (:require [integrant.core :as ig]
+            [duct.database.sql :as sql]
+            [hikaricp.core :as hikaricp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmethod ig/init-key :duct.database.sql/hikaricp [_ options]
+  (sql/->Boundary {:datasource (hikaricp/make-datasource options)}))
+
+(defmethod ig/halt-key! :duct.database.sql/hikaricp [_ {:keys [spec]}]
+  (hikaricp/close-datasource (:datasource spec)))
