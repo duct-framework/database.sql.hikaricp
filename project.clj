5ddeb02d5abc8cf42c309d1d12ac9428cab58bee
James Reeves <jreeves@weavejester.com>
2017-04-15 13:39:09 +0100

    Update database.sql dependency to 0.1.0

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [duct/database.sql "0.1.0-SNAPSHOT"]
+                 [duct/database.sql "0.1.0"]
                  [hikari-cp "1.7.5"]])
