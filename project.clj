2bb3d7cca2e4587ca0a2f8e4b8162b9bc56243ee
James Reeves <jreeves@weavejester.com>
2017-04-15 13:43:14 +0100

    Add missing Integrant dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/database.sql "0.1.0"]
-                 [hikari-cp "1.7.5"]])
+                 [hikari-cp "1.7.5"]
+                 [integrant "0.3.3"]])
