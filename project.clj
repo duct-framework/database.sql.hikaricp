60bbd4b1911e8afb3aff0a38a9c73b29bc1d9e39
James Reeves <jreeves@weavejester.com>
2017-04-15 15:16:58 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/database.sql.hikaricp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for a SQL database connection pool"
+  :url "https://github.com/duct-framework/database.sql.hikaricp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
