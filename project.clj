f63c8e08645d08e2dab05c14afb05c3218ddce34
James Reeves <jreeves@weavejester.com>
2017-08-21 22:13:25 +0100

    Update database dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/core "0.6.0"]
                  [duct/database.sql "0.1.0"]
-                 [hikari-cp "1.7.5"]
+                 [hikari-cp "1.7.6"]
                  [integrant "0.6.1"]
                  [prismatic/schema "1.1.6"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.16.1"]
-                        [org.slf4j/slf4j-nop "1.7.21"]]}})
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.20.0"]
+                        [org.slf4j/slf4j-nop "1.7.25"]]}})
