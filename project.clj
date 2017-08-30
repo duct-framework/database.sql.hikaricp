19e9ceb48cec3d51dbc6853940433e563a76fd0e
James Reeves <jreeves@weavejester.com>
2017-08-30 01:07:01 +0100

    Update duct/logger to 0.2.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/core "0.6.1"]
                  [duct/database.sql "0.1.0"]
-                 [duct/logger "0.2.0"]
+                 [duct/logger "0.2.1"]
                  [hikari-cp "1.7.6"]
                  [integrant "0.6.1"]
                  [prismatic/schema "1.1.6"]
