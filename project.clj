9b5160374dea4f07637062a8508bb5f981bd3a79
James Reeves <jreeves@weavejester.com>
2018-03-13 12:02:00 -0400

    Release 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/database.sql.hikaricp "0.3.2"
+(defproject duct/database.sql.hikaricp "0.3.3"
   :description "Integrant methods for a SQL database connection pool"
   :url "https://github.com/duct-framework/database.sql.hikaricp"
   :license {:name "Eclipse Public License"
