9efb4e3152fb2f1b2d6633110cabeb72b4993a2f
James Reeves <jreeves@weavejester.com>
2018-03-13 12:00:07 -0400

    Update dependencies
    Remove prismatic/schema dependency that is now unnecessary.

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :url "https://github.com/duct-framework/database.sql.hikaricp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [duct/core "0.6.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [duct/core "0.6.2"]
                  [duct/database.sql "0.1.0"]
                  [duct/logger "0.2.1"]
-                 [hikari-cp "1.8.1"]
-                 [integrant "0.6.1"]
-                 [prismatic/schema "1.1.7"]
-                 [net.ttddyy/datasource-proxy "1.4.4"]]
+                 [hikari-cp "2.2.0"]
+                 [integrant "0.6.3"]
+                 [net.ttddyy/datasource-proxy "1.4.7"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.20.1"]
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.21.0.1"]
                         [org.slf4j/slf4j-nop "1.7.25"]
-                        [org.clojure/java.jdbc "0.7.3"]]}})
+                        [org.clojure/java.jdbc "0.7.5"]]}})
