7ac0263b92defbd9d6fb775a0b72cf6abfe065b2
James Reeves <jreeves@weavejester.com>
2019-01-05 18:09:04 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/duct-framework/database.sql.hikaricp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [duct/core "0.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [duct/core "0.7.0"]
                  [duct/database.sql "0.1.0"]
-                 [duct/logger "0.2.1"]
-                 [hikari-cp "2.4.0"]
-                 [integrant "0.7.0-alpha2"]
-                 [net.ttddyy/datasource-proxy "1.4.9"]]
+                 [duct/logger "0.3.0"]
+                 [hikari-cp "2.6.0"]
+                 [integrant "0.7.0"]
+                 [net.ttddyy/datasource-proxy "1.5"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.23.1"]
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.25.2"]
                         [org.slf4j/slf4j-nop "1.7.25"]
-                        [org.clojure/java.jdbc "0.7.6"]]}})
+                        [org.clojure/java.jdbc "0.7.8"]]}})
