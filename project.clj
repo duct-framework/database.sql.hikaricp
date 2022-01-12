3d31175a25dca3eadeed53aee6cfd0363e7b9141
tami5 <kkharji@protonmail.com>
2022-01-12 12:11:26 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [duct/core "0.7.0"]
+                 [duct/core "0.8.0"]
                  [duct/database.sql "0.1.0"]
                  [duct/logger "0.3.0"]
-                 [hikari-cp "2.6.0"]
-                 [integrant "0.7.0"]
-                 [net.ttddyy/datasource-proxy "1.5"]]
+                 [hikari-cp "2.13.0"]
+                 [integrant "0.8.0"]
+                 [net.ttddyy/datasource-proxy "1.7"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.25.2"]
-                        [org.slf4j/slf4j-nop "1.7.25"]
-                        [org.clojure/java.jdbc "0.7.8"]]}})
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.36.0.3"]
+                        [org.slf4j/slf4j-nop "1.7.32"]
+                        [org.clojure/java.jdbc "0.7.12"]]}})
